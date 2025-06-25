df612cdcbc7578ce09363d0fbd16709b8e707b70
Mohib Ali <alimohib025@gmail.com>
2025-06-25 19:02:12 +0500

    Add Navbar and its styling and Add hero section and some styling of it.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#E8E8E8',   // full-site background
+        surface: '#F5F5F5',      // cards, containers
+        textPrimary: '#0C131C',  // main headings/text
+        textSecondary: '#263A53'// subtext/descriptions
+      },
+    },
+  },
+  plugins: [],
+}
+
