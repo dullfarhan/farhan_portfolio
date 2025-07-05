bd176177c2c7f7f6dc799c4e07cc0d9359903513
dullfarhan <dullfarhan@gmail.com>
2025-07-05 18:38:36 +0500

    add love latters and font api and fix contact sizing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
+import { defineConfig, fontProviders } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Inter",
+        weights: [400, 500, 600, 700],
+        display: "swap",
+        cssVariable: "--font-inter",
+      },
+    ],
+  },
 });
+
+
