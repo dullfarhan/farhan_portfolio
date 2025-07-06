1c4361342336ba1a4aeacc6bc14c4c30d63b2ac5
dullfarhan <dullfarhan@gmail.com>
2025-07-07 01:59:08 +0500

    add robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig, fontProviders } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://farhan.info',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), robotsTxt()],
   experimental: {
     fonts: [
       {
